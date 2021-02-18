47adb9ae8f02d286ca5160c64471479c37777133
NBiTech <nagy.benedek003@gmail.com>
2021-02-18 10:05:02 +0100

    Added classes and ETH tracking
    I added seperate classes for each crypto and another for price quering. Clenup still not finished.

--- CryptoRack/CryptoRack/Program.cs
+++ CryptoRack/CryptoRack/Program.cs
@@ -6,15 +6,14 @@ namespace CryptoRack
 {
     class Program
     {
-        public class Prices
-        {
-            public double USD { get; set; }
-        }
-
         static void Main(string[] args)
         {
+            BTCdata BTC = new BTCdata();
+            ETHdata ETH = new ETHdata();
             string json;
-            double prevBTC = 0;
+            double percentileout = 0;
+            double diffout = 0;
+            using (var web = new System.Net.WebClient())
             Console.WriteLine("\tCOIN\tPRICE\t[CHANGE]\t(CHANGE%)");
             for (; ;)
             {
@@ -23,14 +22,14 @@ namespace CryptoRack
                     var url = @"https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD&api_key=fe78af9dcc7d50bfca734160306572698d9a62690be9492c334f626ec4afdec0";
                     json = web.DownloadString(url);
                 }
-                Prices BTC = JsonConvert.DeserializeObject<Prices>(json);
-                BTC.USD = Math.Round(BTC.USD);
+                INPUTdata INBTC = JsonConvert.DeserializeObject<INPUTdata>(json);
+                BTC.RoundUSD = (int)Math.Round(INBTC.USD);
 
-                if (BTC.USD != prevBTC)
+                if (BTC.RoundUSD != BTC.prevUSD)
                 {
                     Console.ForegroundColor = ConsoleColor.DarkYellow;
                     Console.Write("\tBTC:   ");
-                    if (BTC.USD < prevBTC)
+                    if (BTC.RoundUSD < BTC.prevUSD)
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
                     }
@@ -38,13 +37,49 @@ namespace CryptoRack
                     {
                         Console.ForegroundColor = ConsoleColor.Green;
                     }
-                    Console.Write(BTC.USD + "$   ");
-                    Console.Write("[" + (BTC.USD - prevBTC) + "$]          ");
-                    double percentileout = (BTC.USD / prevBTC * 100 - 100);
-                    Console.WriteLine("(" + Math.Round((BTC.USD / prevBTC * 100 - 100), 3, MidpointRounding.AwayFromZero) + "%)   ");
-                    prevBTC = BTC.USD;
+                    Console.Write(BTC.RoundUSD + "$   ");
+                    if (BTC.prevUSD != 0)
+                    {
+                        diffout = BTC.RoundUSD - BTC.prevUSD;
+                        percentileout = Math.Round((BTC.RoundUSD / BTC.prevUSD * 100 - 100), 3, MidpointRounding.AwayFromZero);
+                    }
+                    Console.Write("[" + diffout + "$]          ");
+                    Console.WriteLine("(" + percentileout + "%)   ");
+                    BTC.prevUSD = BTC.RoundUSD;
                 }
-                Thread.Sleep(5000);
+                Thread.Sleep(1000);
+                
+                using (var web = new System.Net.WebClient())
+                {
+                    var url = @"https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD&api_key=fe78af9dcc7d50bfca734160306572698d9a62690be9492c334f626ec4afdec0";
+                    json = web.DownloadString(url);
+                }
+                INPUTdata INETH = JsonConvert.DeserializeObject<INPUTdata>(json);
+                ETH.RoundUSD = (int)Math.Round(INETH.USD);
+
+                if (ETH.RoundUSD != ETH.prevUSD)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    Console.Write("\tETH:   ");
+                    if (ETH.RoundUSD < ETH.prevUSD)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    Console.Write(ETH.RoundUSD + "$   ");
+                    if (ETH.prevUSD != 0) 
+                    {
+                        diffout = ETH.RoundUSD - ETH.prevUSD;
+                        percentileout = Math.Round((ETH.RoundUSD / ETH.prevUSD * 100 - 100), 3, MidpointRounding.AwayFromZero); 
+                    }
+                    Console.Write("[" + diffout + "$]          ");
+                    Console.WriteLine("(" + percentileout + "%)   ");
+                    ETH.prevUSD = ETH.RoundUSD;
+                }
+                Thread.Sleep(1000);
             }
         }
     }
